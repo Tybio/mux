1fd1abb4a4dcffa44e5f9662d1a8dbaf8561132b
Tybio <tybio@mux.net>
2012-12-29 18:09:56 -0500

    Changed logging

--- newznab_cron.sh
+++ newznab_cron.sh
@@ -53,7 +53,7 @@ else
 	OPT_INT=43200
 	#
 	# Log file
-	LOG="/var/log/newznab_cron.log"
+	LOG_FILE="/var/log/newznab_cron.log"
 	#
 	# Debugging, leave off unless you need it
 	#set -xv
@@ -79,6 +79,20 @@ done
 [ "$HELP" ] && echo "`basename $0 .sh` [-q|h]"
 [ "$HELP" ] && exit 1
 
+function log {
+	# If there are parameters read from parameters
+	if [ $# -gt 0 ]; then
+		echo "[$(date +"%D %T")] $@" >> $LOG_FILE
+		[ $DEBUG ] && echo "[$(date +"%D %T")] $@"
+	else 
+    # If there are no parameters read from stdin
+	while read data
+		do
+			echo "[$(date +"%D %T")] $data" >> $LOG_FILE 
+			db "$data"
+		done
+	fi
+}
 #####################
 #
 # Check to see if the config file wants to enable options
@@ -97,33 +111,33 @@ trap "rm -f ${LOCKFILE}; exit" INT TERM
 if [ -e ${LOCKFILE} ]; then
         LOCKFILE_AGE=$(find ${LOCKFILE} -mmin +119)
         if [ -n $LOCKFILE_AGE ];then
-                [ ! "$QUIET" ] && echo "ERROR: $LOCKFILE found, exiting"
+                log "ERROR: $LOCKFILE found, exiting"
                 exit
         else
-                echo "ERROR: $LOCKFILE is $LOCKFILE_AGE, removing it and continuing"
+                log"ERROR: $LOCKFILE is $LOCKFILE_AGE, removing it and continuing"
                 kill -TERM -`cat ${LOCKFILE}`
                 echo $$ > ${LOCKFILE}
         fi
 else
-	[ ! "$QUIET" ] && echo "INFO: Creating lock file"
+	log echo "INFO: Creating lock file"
 	echo $$ > ${LOCKFILE}
 fi
 
 if [ $IMP ]; then
 	if [ ! -d "$IMPORT_DIR" ]; then
-		echo "ERROR: $IMPORT_DIR does not exist, create or remove import option"
+		log "ERROR: $IMPORT_DIR does not exist, create or remove import option"
 		exit 1
 	fi
 fi
 
-[ ! "$QUIET" ] && echo "INFO: Setting Variables"
+log echo "INFO: Setting Variables"
 
 NEWZNAB_PATH="$NEWZNAB_BASE/misc/update_scripts"
 NEWZNAB_LAST_OPT="/tmp/nn-opt-last.txt"
 PHPBIN="/usr/bin/php"
 
 if [ ! -e $NEWZNAB_LAST_OPT ]; then
-	[ ! "$QUIET" ] && echo "INFO: $NEWZNAB_LAST_OPT not found, creating."
+	log "INFO: $NEWZNAB_LAST_OPT not found, creating."
 	echo "$CURRTIME" > /tmp/nn-opt-last.txt
 	DOOPT=1
 else
@@ -131,56 +145,55 @@ else
 	DIFF=$((CURRTIME - LASTOPT))
 	if [ "$DIFF" -gt $OPT_INT ] || [ "$DIFF" -lt 1 ]; then
 		DOOPT=1
-		[ ! "$QUIET" ] && echo "INFO: Timer expired, flagging to run Optimization."
+		log "INFO: Timer expired, flagging to run Optimization."
 	else
 		NEXTOPT=$((OPT_INT - DIFF))
 		NEXTOPTM=$(($NEXTOPT/60))
-		[ ! "$QUIET" ] && echo "INFO: Skipped Optimization, timer expires in $NEXTOPTM minutes"
+		log "INFO: Skipped Optimization, timer expires in $NEXTOPTM minutes"
 	fi
 		
 fi
 
 cd ${NEWZNAB_PATH}
 if [ $THREAD ]; then
-	echo "INFO: Updating binaries (Threaded)"
-	$PHPBIN ${NEWZNAB_PATH}/update_binaries_threaded.php >> $LOG 2>&1
+	log "INFO: Updating binaries (Threaded)"
+	$PHPBIN ${NEWZNAB_PATH}/update_binaries_threaded.php | log
 else
-	echo "INFO: Updating binaries"
-	$PHPBIN ${NEWZNAB_PATH}/update_binaries.php >> $LOG 2>&1
+	log "INFO: Updating binaries"
+	$PHPBIN ${NEWZNAB_PATH}/update_binaries.php | log
 fi
 
 [ ! "$QUIET" ] && echo "INFO: Updating releases"
-$PHPBIN ${NEWZNAB_PATH}/update_releases.php >> $LOG 2>&1
+$PHPBIN ${NEWZNAB_PATH}/update_releases.php | log
 if [ $IMP ]; then
 	IMP_NZB_C=`ls -alh ${IMPORT_DIR} | wc -l`
-	[ ! "$QUIET" ] && echo "INFO: Importing NZBs, $IMP_NZB_C waiting."
-	$PHPBIN ${NEWZNAB_BASE}/www/admin/nzb-importmodified.php ${IMPORT_DIR} true >> $LOG 2>&1
+	log "INFO: Importing NZBs, $IMP_NZB_C waiting."
+	$PHPBIN ${NEWZNAB_BASE}/www/admin/nzb-importmodified.php ${IMPORT_DIR} true | log
 	IMP_NZB_P=`ls -alh ${IMPORT_DIR} | wc -l`
-	[ ! "$QUIET" ] && echo "INFO: Imported NZBs, $IMP_NZB_P left."
-	[ ! "$QUIET" ] && echo "INFO: Updating releases"
-	$PHPBIN ${NEWZNAB_PATH}/update_releases.php >> $LOG 2>&1
+	log "INFO: Imported NZBs, $IMP_NZB_P left."
+	log "INFO: Updating releases"
+	$PHPBIN ${NEWZNAB_PATH}/update_releases.php | log
 fi
 
 if [ $DOOPT ]; then
-	echo "INFO: Starting optimization..." >> $LOG
-	[ ! "$QUIET" ] && echo "INFO: Optimizing DB"
-	$PHPBIN ${NEWZNAB_PATH}/optimise_db.php >> $LOG 2>&1
-	[ ! "$QUIET" ] && echo "INFO: Getting TV Schedule"
-	$PHPBIN ${NEWZNAB_PATH}/update_tvschedule.php >> $LOG 2>&1
-	[ ! "$QUIET" ] && echo "INFO: Getting Movie Times"
-	$PHPBIN ${NEWZNAB_PATH}/update_theaters.php >> $LOG 2>&1
+	log "INFO: Starting optimization..."
+	log "INFO: Optimizing DB"
+	$PHPBIN ${NEWZNAB_PATH}/optimise_db.php | log
+	log INFO: Getting TV Schedule"
+	$PHPBIN ${NEWZNAB_PATH}/update_tvschedule.php | log
+	log INFO: Getting Movie Times"
+	$PHPBIN ${NEWZNAB_PATH}/update_theaters.php | log
 	if [ $PP ]; then
-		[ ! "$QUIET" ] && echo "INFO: Updating Release Parsing"
-		$PHPBIN ${NEWZNAB_BASE}/misc/testing/update_parsing.php >> $LOG 2>&1
+		log "INFO: Updating Release Parsing"
+		$PHPBIN ${NEWZNAB_BASE}/misc/testing/update_parsing.php | log
 	fi
-	[ ! "$QUIET" ] && echo "INFO: Setting timestamp"
+	log "INFO: Setting timestamp"
 	echo "$CURRTIME" > /tmp/nn-opt-last.txt
 fi
 
-[ ! "$QUIET" ] && echo "INFO: Finished, exiting."
 EDATE=`date`
 ETIME=`date +%s`
 RUNTIME=$(($ETIME - $CURRTIME))
 RMIN=$(($RUNTIME/60))
-echo "INFO: Finished at $EDATE, taking $RMIN minutes, exiting." >> $LOG
+echo "INFO: Finished at $EDATE, taking $RMIN minutes, exiting." | log
 rm $LOCKFILE
